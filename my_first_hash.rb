95b59c95002f2a0d16ee071a2dad198559ec38b1
Tracy Reuther <tracyreuther@gmail.com>
2019-06-06 22:58:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+your_hash = {"first name" => "Tracy", "middle name" => "Lynn", "last name" => "Reuther"}
   
 end
 
